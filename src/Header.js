f866f4767e71e4cb9b78d4ab38e94a923cba45cd
ocpuntocom <81719509+ocpuntocom@users.noreply.github.com>
2023-05-07 20:45:44 -0400

    1 file modified

--- src/Header.js
+++ src/Header.js
@@ -2,8 +2,8 @@ import React from 'react';
 
 export function Header(props) {
   let reloj1 = Date().toLocaleString();
-  setInterval(() =>{
+  setInterval(() => {
     reloj1 = Date().toLocaleString();
-  },1000);
+  }, 1000);
   return <h1 className="orange"> Hello {props.name} {props.apellido} {reloj1} </h1> ;
 }
